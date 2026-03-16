3fc8781662a5b6071b65d39c349aae21b2627057
gtrofino <guitrofino@gmail.com>
2026-03-16 20:42:16 -0300

    feat: add deadband to PWM minimum and maximum

--- lib/drivers/pwm/pwm.cpp
+++ lib/drivers/pwm/pwm.cpp
@@ -3,11 +3,11 @@
 
 
 pwm_pulse_norm_t IRAM_ATTR pwm_pulse_us_normalize(pwm_pulse_t pulse_us) {
-    if (pulse_us > (pwm_pulse_t) PWM_MAXIMUM_US) {
+    if (pulse_us > (pwm_pulse_t) PWM_MAXIMUM_US - PWM_DEADBAND_US) {
         return (pwm_pulse_norm_t) PWM_MAXIMUM_US;
     }
 
-    if (pulse_us < (pwm_pulse_t) PWM_MINIMUM_US) {
+    if (pulse_us < (pwm_pulse_t) PWM_MINIMUM_US + PWM_DEADBAND_US) {
         return (pwm_pulse_norm_t) PWM_MINIMUM_US;
     }
 
