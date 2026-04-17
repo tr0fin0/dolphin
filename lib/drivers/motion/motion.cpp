db6909eda1a68d47aa28d7ab939108ff4aaaa80c
gtrofino <guitrofino@gmail.com>
2026-04-16 21:58:55 -0300

    chore: delays corrections

--- lib/drivers/motion/motion.cpp
+++ lib/drivers/motion/motion.cpp
@@ -4,52 +4,54 @@
 #include "motor/motor.h"
 #include "pwm/pwm.h"
 
+int8_t offset = 50;
 
 
 void motion_rotation(angle_t angle) {
-    if (angle == -45) {
-        esc_set_pwm((pwm_pulse_norm_t) PWM_MINIMUM_US, MOTOR_L);
-        esc_set_pwm((pwm_pulse_norm_t) PWM_MINIMUM_US, MOTOR_R);
-
-        delay(40);
-    } else if (angle == +45) {
-        esc_set_pwm((pwm_pulse_norm_t) PWM_MAXIMUM_US, MOTOR_L);
+    if (angle == +45) {
+        esc_set_pwm((pwm_pulse_norm_t) PWM_MAXIMUM_US - offset, MOTOR_L);
         esc_set_pwm((pwm_pulse_norm_t) PWM_MAXIMUM_US, MOTOR_R);
 
-        delay(60);
+        delay(24);
+    } else if (angle == -45) {
+        esc_set_pwm((pwm_pulse_norm_t) PWM_MINIMUM_US + offset, MOTOR_L);
+        esc_set_pwm((pwm_pulse_norm_t) PWM_MINIMUM_US, MOTOR_R);
+
+        delay(30);
     } else if (angle == -90) {
-        esc_set_pwm((pwm_pulse_norm_t) PWM_MINIMUM_US, MOTOR_L);
+        esc_set_pwm((pwm_pulse_norm_t) PWM_MINIMUM_US + offset, MOTOR_L);
         esc_set_pwm((pwm_pulse_norm_t) PWM_MINIMUM_US, MOTOR_R);
 
-        delay(60);
+        delay(36);
     } else if (angle == +90) {
-        esc_set_pwm((pwm_pulse_norm_t) PWM_MAXIMUM_US, MOTOR_L);
+        esc_set_pwm((pwm_pulse_norm_t) PWM_MAXIMUM_US - offset, MOTOR_L);
         esc_set_pwm((pwm_pulse_norm_t) PWM_MAXIMUM_US, MOTOR_R);
 
-        delay(100);
-    } else if (angle == -180) {
-        esc_set_pwm((pwm_pulse_norm_t) PWM_MINIMUM_US, MOTOR_L);
-        esc_set_pwm((pwm_pulse_norm_t) PWM_MINIMUM_US, MOTOR_R);
-
-        delay(125);
+        delay(36);
     } else if (angle == +180) {
-        esc_set_pwm((pwm_pulse_norm_t) PWM_MAXIMUM_US, MOTOR_L);
+        esc_set_pwm((pwm_pulse_norm_t) PWM_MAXIMUM_US - offset, MOTOR_L);
         esc_set_pwm((pwm_pulse_norm_t) PWM_MAXIMUM_US, MOTOR_R);
 
-        delay(150);
+        delay(72);
     }
+
+    motors_set_pwm_neutral();
+    delay(50);
 }
 
 void motion_translation(distance_t distance) {
     if (distance == -50) {
-        esc_set_pwm((pwm_pulse_norm_t) PWM_MINIMUM_US + 120, MOTOR_L);
+        esc_set_pwm((pwm_pulse_norm_t) PWM_MINIMUM_US+75, MOTOR_L);
         esc_set_pwm((pwm_pulse_norm_t) PWM_MAXIMUM_US, MOTOR_R);
 
-        delay(500);
+        delay(135);
     } else if (distance == +50) {
         esc_set_pwm((pwm_pulse_norm_t) PWM_MAXIMUM_US, MOTOR_L);
-        esc_set_pwm((pwm_pulse_norm_t) PWM_MINIMUM_US + 75, MOTOR_R);
+        esc_set_pwm((pwm_pulse_norm_t) PWM_MINIMUM_US+25, MOTOR_R);
 
-        delay(200);
+        delay(115);
     }
+
+    motors_set_pwm_neutral();
+    delay(50);
 }
