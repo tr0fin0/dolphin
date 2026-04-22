de42fd88673d3009cb06c7cb0df03d2b24fb0a49
gtrofino <guitrofino@gmail.com>
2026-04-21 21:28:56 -0300

    feat: add boot state files

---
+++ lib/fsm/states/boot.h
@@ -0,0 +1,22 @@
+#ifndef __BOOT_H__
+#define __BOOT_H__
+
+
+
+/**
+ * @brief Boot state entry handler.
+ *
+ * Sets visual indication upon entering `STATE_BOOT`.
+ */
+void boot_entry(void);
+
+/**
+ * @brief Boot state run handler.
+ *
+ * First .
+ */
+void boot_run(void);
+
+
+
+#endif  /* __BOOT_H__ */
