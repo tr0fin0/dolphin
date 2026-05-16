3e009140726a8ec4634620f9042f986593c090ba
gtrofino <guitrofino@gmail.com>
2026-05-16 20:39:40 -0300

    chore: remove firmware drafts

--- components/pid/include/pid.h
+++
@@ -1,3 +0,0 @@
-// https://medium.com/@soham.phanse/a-simple-pid-controller-in-c-0570277a989a
-// https://www.youtube.com/watch?v=0lYtHHOuH1k
-// https://en.wikipedia.org/wiki/Proportional%E2%80%93integral%E2%80%93derivative_controller
