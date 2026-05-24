4ed4d2551b219520719ff51077e84c8b0e9160a1
gtrofino <guitrofino@gmail.com>
2026-05-24 10:04:03 -0300

    refactor: rename escs configuration

--- components/esc/esc.c
+++ components/esc/esc.c
@@ -2,7 +2,7 @@
 #include "esp_err.h"
 #include "logging.h"
 
-static esc_config_t controllers[NUMBER_OF_ESCS] = {
+static esc_config_t escs[NUMBER_OF_ESCS] = {
     [ESC_L] = {
         .name       = "ESC_L",
         .pin        = PIN_ESC_L,
@@ -64,12 +64,13 @@ void esc_init() {
             .flags.update_cmp_on_tez = true,
         };
         ret = mcpwm_new_comparator(
-            operator, &comparator_config, &controllers[i].comparator
+            operator, &comparator_config, &escs[i].comparator
         );
         if (ret != ESP_OK) {
             LOG_E(
                 "%s MCPWM comparator creation failed with error %s.",
-                controllers[i].name, esp_err_to_name(ret)
+                escs[i].name,
+                esp_err_to_name(ret)
             );
 
             return;
@@ -77,13 +78,14 @@ void esc_init() {
 
         mcpwm_gen_handle_t generator = NULL;
         mcpwm_generator_config_t generator_config = {
-            .gen_gpio_num = controllers[i].pin,
+            .gen_gpio_num = escs[i].pin,
         };
         ret = mcpwm_new_generator(operator, &generator_config, &generator);
         if (ret != ESP_OK) {
             LOG_E(
                 "%s MCPWM generator creation failed with error %s",
-                controllers[i].name, esp_err_to_name(ret)
+                escs[i].name,
+                esp_err_to_name(ret)
             );
 
             return;
@@ -100,7 +102,8 @@ void esc_init() {
         if (ret != ESP_OK) {
             LOG_E(
                 "%s MCPWM generator set action on timer failed with error %s",
-                controllers[i].name, esp_err_to_name(ret)
+                escs[i].name,
+                esp_err_to_name(ret)
             );
 
             return;
@@ -110,33 +113,38 @@ void esc_init() {
             generator,
             MCPWM_GEN_COMPARE_EVENT_ACTION(
                 MCPWM_TIMER_DIRECTION_UP,
-                controllers[i].comparator,
+                escs[i].comparator,
                 MCPWM_GEN_ACTION_LOW
             )
         );
         if (ret != ESP_OK) {
             LOG_E(
                 "%s MCPWM generator set action on compare failed with error %s",
-                controllers[i].name, esp_err_to_name(ret)
+                escs[i].name,
+                esp_err_to_name(ret)
             );
 
             return;
         };
 
         ret = mcpwm_comparator_set_compare_value(
-            controllers[i].comparator, PWM_NEUTRAL_US
+            escs[i].comparator,
+            PWM_NEUTRAL_US
         );
         if (ret != ESP_OK) {
             LOG_E(
                 "%s compare value set failed with error %s.",
-                controllers[i].name, esp_err_to_name(ret)
+                escs[i].name,
+                esp_err_to_name(ret)
             );
 
             return;
         }
 
         LOG_I(
-            "%s initialized on pin %d.", controllers[i].name, controllers[i].pin
+            "%s initialized on pin %d.",
+            escs[i].name,
+            escs[i].pin
         );
     }
 
@@ -159,18 +167,18 @@ void esc_set_pwm(esc_t esc, pwm_t pwm) {
     pwm_norm_t pwm_norm = pwm_normalize(pwm);
 
     esp_err_t ret = mcpwm_comparator_set_compare_value(
-        controllers[esc].comparator, pwm_norm
+        escs[esc].comparator, pwm_norm
     );
     if (ret != ESP_OK) {
         LOG_E(
             "%s compare value set failed with error %s.",
-            controllers[esc].name, esp_err_to_name(ret)
+            escs[esc].name, esp_err_to_name(ret)
         );
 
         return;
     }
 
-    LOG_V("%s duty cycle set to %d us.", controllers[esc].name, pwm_norm);
+    LOG_V("%s duty cycle set to %d us.", escs[esc].name, pwm_norm);
 }
 
 void esc_set_pwm_mix(pwm_norm_t pwms[NUMBER_OF_ESCS]) {
