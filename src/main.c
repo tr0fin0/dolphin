802bd0e1841554ef0cc3e13cd66121ea5e83b363
Guilherme Nunes Trofino <guitrofino@gmail.com>
2026-07-30 18:49:34 -0300

    tmp encoder validation

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include "encoder.h"
 #include "fsm.h"
 #include "led.h"
 #include "logging.h"
@@ -7,15 +8,22 @@
 void app_main(void) {
     logging_init_async();
 
-    odometry_init();
-    led_init();
+    encoder_init();
     fsm_init();
+    led_init();
+    odometry_init();
 
     while (1) {
         odometry_step();
         led_step();
         fsm_step();
 
-        vTaskDelay(1);
+        LOG_E(
+            "encoder values %.04f | %.04f",
+            encoder_read_angle(ENCODER_L),
+            encoder_read_angle(ENCODER_R)
+        );
+
+        vTaskDelay(10);
     }
 }
