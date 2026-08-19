d81b2b3e88521b03b9b3993baf3198934e20624b
Guilherme Nunes Trofino <guitrofino@gmail.com>
2026-08-19 15:38:16 -0300

    refactor(docs): attack.h update doxygen references

--- components/fsm/states/include/attack.h
+++ components/fsm/states/include/attack.h
@@ -1,6 +1,6 @@
 /**
  * @file attack.h
- * @brief `STATE_ATTACK` definition of the FSM callback functions `on_exit()`
+ * @brief @ref STATE_ATTACK definition of the FSM callback functions `on_exit()`
  * and `on_run()`.
  *
  * @author Guilherme Nunes Trofino
@@ -10,23 +10,23 @@
 #pragma once
 
 /**
- * @brief Run handler for `STATE_ATTACK`.
+ * @brief Run handler for @ref STATE_ATTACK.
  *
  * While the Radio Controller is connected, autonomously push the adversary.
  *
  * @note
- * - Transition to `STATE_SEARCH` if the adversary is lost.
+ * - Transition to @ref STATE_SEARCH if the adversary is lost.
  *
- * - Transition to `STATE_SAFE` if the Radio Controller is disconnected.
+ * - Transition to @ref STATE_SAFE if the Radio Controller is disconnected.
  *
- * - Transition to `STATE_SURVIVE` if the dojo edge is detected.
+ * - Transition to @ref STATE_SURVIVE if the dojo edge is detected.
  */
 void attack_run(void);
 
 /**
- * @brief Exit handler for `STATE_ATTACK`.
+ * @brief Exit handler for @ref STATE_ATTACK.
  *
- * Ensures both motors are driven to neutral PWM, preventing unintended motion
- * during transition.
+ * Ensures both motors are driven to @ref PWM_NEUTRAL_US, preventing unintended
+ * motion during transition.
  */
 void attack_exit(void);
