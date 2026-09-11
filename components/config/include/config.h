05fc5e3cfdb23cf07ccd2be36d24635790ac260a
Guilherme Nunes Trofino <guitrofino@gmail.com>
2026-09-11 13:59:43 -0300

    chore: config.h remove mainboard version type

--- components/config/include/config.h
+++ components/config/include/config.h
@@ -20,16 +20,6 @@ typedef enum config_control_mode {
 } config_control_mode_t;
 
 /**
- * @brief Available mainboard versions.
- *
- * @note Basically modifying the @ref pin enumerate.
- */
-typedef enum config_mainboard_version {
-    CONFIG_MAINBOARD_V1 = 0,    /**< Mainboard version 1 made in Altium. */
-    CONFIG_MAINBOARD_V2,        /**< Mainboard version 2 made in KiCAD. */
-} config_mainboard_version_t;
-
-/**
  * @def CONFIG_CONTROL_MODE
  * @brief Current control approach.
  *
